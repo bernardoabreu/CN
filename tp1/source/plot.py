af4b54824a757c842a42d3371e46663b0797d3db
bernardoabreu <bernardoaabreu@gmail.com>
2017-09-19 00:07:29 -0300

    Update plot

--- tp1/source/plot.py
+++ tp1/source/plot.py
@@ -1,76 +1,49 @@
+#!/usr/bin/env  python
+
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+# import pandas as pd
 from pandas import DataFrame
 from sys import argv
 
 
 def loadtxt(file, delimiter=','):
     with open(file, 'r') as f:
-        return [list(map(float, line[:-1].split(delimiter))) for line in f]
+        return np.array([list(map(float, line[:-1].split(delimiter)))
+                        for line in f])
 
 
-def read_csv(outfile):
-    # a = np.genfromtxt(outfile, delimiter=',', names=True,)
-    #                   converters={'inf': float('inf')})
-    # print a
-    # print a.dtype.names
-    means = loadtxt(outfile)
-
-    a = np.array(means)
-    print a
-
-    means_of_means = []
-    stds = []
-    max_min = []
+def lineplot(a, y_label):
+    means = []
     for column in a.T:
-        means_of_means.append(np.mean(column))
-        stds.append(np.std(column))
-        if np.mean(column) - np.std(column) < 0:
-            print 'cu'
-        # max_.append(np.max(column))
-        # min_.append(np.min(column))
+        means.append(np.mean(column))
+    a_means = np.array(means)
+    plt.plot(np.arange(len(a_means)), a_means)
+    plt.xlabel('generations')
+    plt.ylabel(y_label)
+    plt.title(y_label + ' x ' + 'generations')
+    plt.grid(True)
+    plt.show()
 
-    a_means = np.array(means_of_means)
-    a_stds = np.array(stds)
-    # a_max_min = np.array(max_min)
 
-    # construct some data like what you have:
-    # x = np.random.randn(100, 8)
-    # mins = x.min(0)
-    # maxes = x.max(0)
-    # means = x.mean(0)
-    # std = x.std(0)
-
-    print stds
-    print max_min
-
-    df = DataFrame(means)
+def boxplot(a, y_label):
+    df = DataFrame(a)
     plt.figure()
     df.boxplot()
-
-    # create stacked errorbars:
-    # plt.errorbar(np.arange(8), means, [means - mins, maxes - means],
-    #              fmt='.k', ecolor='gray', lw=1)
-    # plt.errorbar(np.arange(8), means, std, fmt='ok', lw=5)
-    # plt.xlim(-1, 8)
-    # plt
-    # plt.errorbar(np.arange(len(a_means)), a_means, a_max_min,
-    #              fmt='.k', ecolor='gray', lw=1)
-    # plt.errorbar(np.arange(len(a_means)), a_means, a_stds, fmt='ok', lw=5)
-    # plt.xlim(-1, 8)
-
-    # n = np.loadtxt(outfile, delimiter=',', skiprows=1)
-    # print n.dtype
-    # print n[:,0]
-    # print n[:,3]
-    # plt.plot(np.arange(len(a_means)), a_means)
     plt.xlabel('generations')
-    # plt.ylabel('Means')
-    # # plt.title(names[3] + ' x ' + names[0])
-    # plt.grid(True)
+    plt.ylabel(y_label)
+    plt.title(y_label + ' x ' + 'generations')
+    plt.grid(True)
     # plt.savefig("test.png")
     plt.show()
 
 
-read_csv(argv[1])
+if __name__ == '__main__':
+
+    in_file = argv[1]
+    label = argv[2]
+
+    a = loadtxt(in_file)
+
+    boxplot(a, label)
+    boxplot(a, label)
