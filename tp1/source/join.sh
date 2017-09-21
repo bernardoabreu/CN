13ad402bae1e0371cb78f673ab83721f5a60b925
bernardoabreu <bernardoaabreu@gmail.com>
2017-09-20 23:45:47 -0300

    add out files for test 3 500

--- tp1/source/join.sh
+++ tp1/source/join.sh
@@ -2,9 +2,16 @@
 
 
 FILE="$1"
+STEP="$2"
 
 touch "out_house__${1}.csv"
 
-cat "out_house_1_10__${1}.csv" >> "out_house__${1}.csv"
-cat "out_house_11_20__${1}.csv" >> "out_house__${1}.csv"
-cat "out_house_21_30__${1}.csv" >> "out_house__${1}.csv"
+
+for i in $(seq 1 $STEP 30); do
+	cat "out_house_${i}_$(($i+$STEP-1))__${1}.csv" >> "out_house__${1}.csv";
+done
+
+
+# cat "out_house_1_10__${1}.csv" >> "out_house__${1}.csv"
+# cat "out_house_11_20__${1}.csv" >> "out_house__${1}.csv"
+# cat "out_house_21_30__${1}.csv" >> "out_house__${1}.csv"
