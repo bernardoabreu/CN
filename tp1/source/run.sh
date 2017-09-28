91cd94c7d83736a7ac5eeffa82be59690ea7251e
bernardoabreu <bernardoaabreu@gmail.com>
2017-09-27 21:40:56 -0300

    Add tests2

--- tp1/source/run.sh
+++ tp1/source/run.sh
@@ -6,8 +6,15 @@ echo $HOST
 PA=$HOME'/CN/tp1'
 TEST_VERSION=7
 DATA=(keijzer-7 keijzer-10)
-SUBDIRS=(keijzer-7 keijzer-10)
+SUBDIRS=(50 100 500)
 OUT=tests2
+
+ELITISM=1
+CROSS=0.9
+MUT=0.05
+TOUR=2
+GEN=100
+POP=500
 
 while getopts 'd:v:o:s:h' opt ; do
   case $opt in
@@ -16,6 +23,9 @@ while getopts 'd:v:o:s:h' opt ; do
     o) OUT=$OPTARG;;
     s) SUBDIRS=("$OPTARG");;
     h) echo "Options: -d datasets (def: ${DATA[*]})";
+       echo "         -v test version (def: ${TEST_VERSION})";
+       echo "         -o output directory (def: ${OUT})";
+       echo "         -s subdirectories (def: ${SUBDIRS[*]})";
        exit 0 ;;
   esac
 done
@@ -30,14 +40,15 @@ echo $$ > "${PA}/norun_${HOST}.pid"
 
 for f in ${DATA[*]}; do
 
-    DIR=$PA/$OUT/$f/$TEST_VERSION
-    mkdir $DIR
+    DIR=$PA/$OUT/$f/$TEST_VERSION/
 
     for SUBDIR in ${SUBDIRS[*]}; do
         mkdir -p $PA/$OUT/$f/$TEST_VERSION/$SUBDIR
 
+        GEN=$SUBDIR
+
         for i in {1..30}; do
-            $PA/source/main.py --elitism 0 --crossover 0.9 --mutation 0.05 --tournament 7 --seed $i --gen 100 --pop_size 500 --train $PA/datasets/$f-train.csv --test $PA/datasets/$f-test.csv --stats "${DIR}${SUBDIR}/out_${f}" --test_out "${DIR}${SUBDIR}/out_${f}" > "${DIR}${SUBDIR}/${f}_${i}"
+            $PA/source/main.py --elitism $ELITISM --crossover $CROSS --mutation $MUT --tournament $TOUR --seed $i --gen $GEN --pop_size $POP --train $PA/datasets/$f-train.csv --test $PA/datasets/$f-test.csv --stats "${DIR}${SUBDIR}/out_${f}" --test_out "${DIR}${SUBDIR}/out_${f}" > "${DIR}${SUBDIR}/${f}_${i}"
         done
     done
 
