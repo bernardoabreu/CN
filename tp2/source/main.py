f1cd0bbab23c1e619e44e85d8ba6a62300f85447
bernardoabreu <bernardoaabreu@gmail.com>
2017-11-01 23:53:25 -0200

    Add tests 1 and 2

--- tp2/source/main.py
+++ tp2/source/main.py
@@ -17,8 +17,8 @@ def main(args):
     num_ants = args.ants
     iterations = args.iterations
     pher = 0.5
-    a = args.a
-    b = args.b
+    a = args.alpha
+    b = args.beta
     decay = args.decay
     seed = args.seed
     stats = args.stats
@@ -36,24 +36,23 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--file', required=True,
+    parser.add_argument('-f', '--file', required=True,
                         help='Input file.')
-    parser.add_argument('--ants', type=int, default=10,
+    parser.add_argument('-n', '--ants', type=int, default=10,
                         help='Number of ants.')
-    parser.add_argument('--iterations', type=int, default=100,
+    parser.add_argument('-i', '--iterations', type=int, default=100,
                         help='Number of iterations.')
-    parser.add_argument('-a', type=int, default=3,
+    parser.add_argument('-a', '--alpha', type=int, default=3,
                         help='Alpha. Influence of the pheromone.')
-    parser.add_argument('-b', type=int, default=1,
+    parser.add_argument('-b', '--beta', type=int, default=1,
                         help='Beta. Influence of the heuristic function.')
-    parser.add_argument('--decay', type=float, default=0.1,
-                        help='Seed for random number generator')
-    parser.add_argument('--seed', type=int, default=None,
-                        help='Seed for random number generator')
+    parser.add_argument('-d', '--decay', type=float, default=0.1,
+                        help='Decay rate of pheromones.')
+    parser.add_argument('-s', '--seed', type=int, default=None,
+                        help='Seed for random number generator.')
     parser.add_argument('--stats', default='',
                         help='File to save statistics.')
 
     args = parser.parse_args()
-    # print(args)
 
     main(args)
---
+++ tp2/source/run.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+HOST=$(hostname)
+
+echo $HOST
+BASE=$HOME'/CN/tp2'
+TEST_VERSION=2
+DATA=(SJC1 SJC2 SJC3b)
+SUBDIRS=(30 60 90)
+OUT=tests
+
+ITER=500
+ANTS=10
+ALPHA=3
+BETA=1
+DECAY=0.1
+
+
+while getopts 'd:v:o:s:h' opt ; do
+  case $opt in
+    d) DATA=("$OPTARG");;
+    v) TEST_VERSION=$OPTARG;;
+    o) OUT=$OPTARG;;
+    s) SUBDIRS=("$OPTARG");;
+    h) echo "Options: -d datasets (def: ${DATA[*]})";
+       echo "         -v test version (def: ${TEST_VERSION})";
+       echo "         -o output directory (def: ${OUT})";
+       echo "         -s subdirectories (def: ${SUBDIRS[*]})";
+       exit 0 ;;
+  esac
+done
+
+
+# skip over the processed options
+shift $((OPTIND-1))
+
+
+echo $$ > "${BASE}/norun_${HOST}.pid"
+
+
+for FILE in ${DATA[*]}; do
+
+    DIR=$BASE/$OUT/$FILE/$TEST_VERSION/
+
+    for SUBDIR in ${SUBDIRS[*]}; do
+        mkdir -p "${DIR}${SUBDIR}"
+
+        ANTS=$SUBDIR
+
+        for i in {1..30}; do
+          echo "${FILE} - ${SUBDIR} ${i}"
+            $BASE/source/main.py -n $ANTS -i $ITER -a $ALPHA -b $BETA --seed $i -f $BASE/dataset/"${FILE}.dat" --stats "${DIR}${SUBDIR}/out_${FILE}" > "${DIR}${SUBDIR}/${FILE}_${i}"
+        done
+    done
+
+done
+
+rm "${BASE}/norun_${HOST}.pid"
